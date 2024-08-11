100b915169c175e3eb185ecbc72ef8506979b99d
Eun24 <dmsghdmstj@g.skku.edu>
2024-08-11 14:21:21 +0900

    notion api를 연동한다

---
+++ .github/notion-api/index.js
@@ -0,0 +1,102 @@
+const axios = require('axios');
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const NOTION_API_KEY = process.env.NOTION_API_KEY;
+const USER_NAME = process.env.USER_NAME;
+const NEW_FILES = process.env.NEW_FILES;
+const NOTION_API_DATABASE_URL = `https://api.notion.com/v1/databases/${DATABASE_URL}/query`;
+const NOTION_API_PAGE_URL = "https://api.notion.com/v1/pages/";
+
+async function run(userName, fileString) {
+  try {
+    console.log(`NEW_FILE_STRING: ${fileString}`);
+    const pages = await getPages();
+
+    filePaths = extractFilePathsFromString(fileString);
+
+    for (const filePath of filePaths) {
+      const problemName = getProblemName(filePath);
+      const targetPage = getTargetPage(pages, problemName);
+
+      if(isEmptyTargetPage(targetPage)){
+        console.log(`"${problemName}"라는 이름을 가진 페이지를 찾을 수 없습니다.`);
+        continue;
+      }
+
+      await updateCheckbox(targetPage, userName);
+
+      console.log(`"${problemName}"의 체크박스를 '체크'했습니다.`);
+    }
+  } catch (error) {
+    console.error('오류 발생:', error.response ? error.response.data : error.message);
+  }
+}
+
+async function updateCheckbox(targetPage, userName) {
+  const updateUrl = getUpdateUrl(targetPage);
+  const updateData = getUpdateData(userName);
+  const headers = getHeaders();
+
+  await axios.patch(updateUrl, updateData, {headers});
+}
+
+function extractFilePathsFromString(fileString) {
+  const filePaths =  fileString.split(',');
+  filePaths.pop();
+
+  return filePaths;
+}
+
+function extractFileNameFromPath(filePath) {
+  const filePathSplit = filePath.split('/');
+  const fileName = filePathSplit[filePathSplit.length - 1];
+
+  return fileName;
+}
+
+function getTargetPage(pages, problemName) {
+  return pages.find(page => page?.properties?.번호?.rich_text[0]?.plain_text === problemName);
+}
+
+function isEmptyTargetPage(targetPage) {
+  return targetPage === undefined;
+}
+
+function getUpdateUrl(targetPage) {
+  return NOTION_API_PAGE_URL + targetPage.id;
+}
+
+function getUpdateData(userName) {
+  return {
+    "properties": {
+      [userName]: {
+        "checkbox": true
+      }
+    }
+  };
+}
+
+async function getPages() {
+  const headers = getHeaders();
+  const response = await axios.post(NOTION_API_DATABASE_URL, {}, {headers});
+  const pages = response.data.results;
+
+  return pages;
+}
+
+function getProblemName(filePath) {
+  const fileName = extractFileNameFromPath(filePath);
+  const problemName = fileName.split('.')[0];
+
+  return problemName;
+}
+
+function getHeaders() {
+  return {
+    "Authorization": `Bearer ${NOTION_API_KEY}`,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+  };
+}
+
+run(USER_NAME, NEW_FILES);
